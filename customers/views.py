c6878d34cb7f2d1aa86fd4b4c137ba5ee1922653
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2023-08-26 16:21:09 +0100

    added registration view

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,6 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
-from customers.serializers import CustomerSerializer
+from customers.serializers import CustomerSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
@@ -42,3 +42,10 @@ def customer(request, id):
             serializer.save()
             return Response({'customer': serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])    
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
