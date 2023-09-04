d8333cd4878da7164741aa9aec4428c2bce6620a
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2023-09-04 01:15:32 +0100

    MUTATION

--- customers/urls.py
+++ customers/urls.py
@@ -20,6 +20,8 @@ from customers import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from graphene_django.views import GraphQLView
 
+from django.views.decorators.csrf import csrf_exempt
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
@@ -27,5 +29,5 @@ urlpatterns = [
     path('api/customers/', views.customers, name='customers'),
     path('api/customers/<int:id>', views.customer, name='customer'),
     path('api/register/', views.register, name='register'), 
-    path('graphql',GraphQLView.as_view(graphiql=True))
+    path('graphql/',csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
